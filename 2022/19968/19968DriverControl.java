2854862bb88ae35a55c7fa35f33563f17f4a88ac
David Schoemehl <davidschoemehl@gmail.com>
2022-09-23 14:29:41 -0500

    Updates for this year's robot

--- 2022/19968/19968DriverControl.java
+++ 2022/19968/19968DriverControl.java
@@ -2,48 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "_19968DriverControl (Blocks to Java)")
 public class _19968DriverControl extends LinearOpMode {
 
-  private DcMotor spinner;
   private DcMotor frontright;
   private DcMotor frontleft;
   private DcMotor backleft;
   private DcMotor backright;
-  private DcMotor lift;
-  private CRServo spokes;
 
   double powerFactor;
   float leftStickY;
-  int liftPosition;
-  boolean upPressed;
   float leftStickX;
   float rightStickX;
-  int liftIncrement;
-  int liftMax;
-  int liftMin;
 
   /**
-   * Describe this function...
-   */
-  private void spinner2() {
-    boolean RightBumper;
-    boolean LeftBumper;
-
-    RightBumper = gamepad2.right_bumper;
-    LeftBumper = gamepad2.left_bumper;
-    spinner.setPower(RightBumper - LeftBumper);
-    telemetry.addData("LeftBumper", LeftBumper);
-    telemetry.addData("RightBumper", RightBumper);
-    telemetry.addData("spinnerpower", spinner.getPower());
-  }
-
-  /**
-   * Describe this function...
+   * This function reads inputs from the joysticks and powers the wheels
    */
   private void drive() {
     float powerFrontLeft;
@@ -57,17 +33,25 @@ public class _19968DriverControl extends LinearOpMode {
     if (gamepad1.y) {
       powerFactor = 0.5;
     }
+
+    //Get stick inputs
     leftStickY = -gamepad1.left_stick_y;
     leftStickX = gamepad1.left_stick_x;
     rightStickX = gamepad1.right_stick_x;
+
+    //Set the power values based on stick input
     powerFrontLeft = leftStickY + leftStickX + rightStickX;
     powerBackLeft = leftStickY + -leftStickX + rightStickX;
     powerFrontRight = leftStickY + (-leftStickX - rightStickX);
     powerBackRight = leftStickY + (leftStickX - rightStickX);
+
+    //Set the power to the motors
     frontright.setPower(powerFactor * powerFrontRight);
     frontleft.setPower(powerFactor * powerFrontLeft);
     backleft.setPower(powerFactor * powerBackLeft);
     backright.setPower(powerFactor * powerBackRight);
+
+    //Print data to the screen
     telemetry.addData("leftStickY", leftStickY);
     telemetry.addData("leftStickX", leftStickX);
     telemetry.addData("rightStickX", rightStickX);
@@ -84,94 +68,32 @@ public class _19968DriverControl extends LinearOpMode {
    */
   @Override
   public void runOpMode() {
-    boolean testRobot;
 
-    spinner = hardwareMap.get(DcMotor.class, "spinner");
-    frontright = hardwareMap.get(DcMotor.class, "front right");
-    frontleft = hardwareMap.get(DcMotor.class, "front left");
-    backleft = hardwareMap.get(DcMotor.class, "back left");
-    backright = hardwareMap.get(DcMotor.class, "back right");
-    lift = hardwareMap.get(DcMotor.class, "lift");
-    spokes = hardwareMap.get(CRServo.class, "spokes");
+    //Map the names from the configuration to the variables
+    frontright = hardwareMap.get(DcMotor.class, "fr_rt");
+    frontleft = hardwareMap.get(DcMotor.class, "fr_lt");
+    backleft = hardwareMap.get(DcMotor.class, "bk_lt");
+    backright = hardwareMap.get(DcMotor.class, "bk_rt");
 
-    // Put initialization blocks here.
-    // These are for the test robot.
+    //Set the directions for the motors
     frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
     frontright.setDirection(DcMotorSimple.Direction.FORWARD);
     backleft.setDirection(DcMotorSimple.Direction.REVERSE);
     backright.setDirection(DcMotorSimple.Direction.FORWARD);
-    lift.setTargetPosition(0);
-    lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    upPressed = false;
-    liftPosition = 0;
-    liftMax = 2400;
-    liftMin = 0;
-    liftIncrement = 800;
+
+    //Initialize the variables
     leftStickX = 0;
     leftStickY = 0;
     rightStickX = 0;
-    testRobot = false;
     powerFactor = 0.5;
+
     waitForStart();
+
+    //This is the main loop. I will call the drive function on a loop until you stop the robot
     if (opModeIsActive()) {
       while (opModeIsActive()) {
-        // Put loop blocks here.
-        spinner2();
-        spokes2();
         drive();
-        lift2();
       }
     }
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void spokes2() {
-    int spokePower;
-
-    if (gamepad2.x) {
-      spokePower = 1;
-    } else if (gamepad2.a) {
-      spokePower = -1;
-    } else {
-      spokePower = 0;
-    }
-    spokes.setPower(spokePower);
-    telemetry.addData("spokePower", spokePower);
-  }
-
-  /**
-   * Describe this function...
-   */
-  private void lift2() {
-    double liftDOWN;
-    boolean downPressed;
-
-    lift.setPower(1);
-    if (gamepad2.dpad_up && !upPressed) {
-      liftPosition = liftPosition + liftIncrement;
-      upPressed = true;
-    } else if (!gamepad2.dpad_up && upPressed) {
-      upPressed = false;
-    } else {
-    }
-    if (gamepad2.dpad_down && !downPressed) {
-      liftPosition = liftPosition - liftIncrement;
-      downPressed = true;
-    } else if (!gamepad2.dpad_down && downPressed) {
-      downPressed = false;
-    } else {
-    }
-    if (liftPosition > liftMax) {
-      liftPosition = liftMax;
-    } else if (liftPosition < liftMin) {
-      liftPosition = liftMin;
-    } else {
-    }
-    lift.setTargetPosition(liftPosition);
-    telemetry.addData("liftPosition", liftPosition);
-    telemetry.addData("lift down", liftDOWN);
   }
 }
